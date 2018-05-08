44648d508befe765e8e5400e5a6ff543bb2cbb7c
Ben Agricola <bagricola@squiz.co.uk>
2018-05-08 17:04:48 +0100

    Fix ipv{4,6}-prefix parsing and tostring functionality
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/lib/yang/value.lua
+++ src/lib/yang/value.lua
@@ -144,18 +144,20 @@ types['ipv4-prefix'] = {
    ctype = 'struct { uint8_t prefix[4]; uint8_t len; }',
    parse = function(str, what)
       local prefix, len = str:match('^([^/]+)/(.*)$')
-      return { ipv4_pton(prefix), util.tointeger(len, 1, 32) }
+      return { assert(ipv4:pton(prefix)), util.tointeger(len, 'len', 1, 32) }
    end,
-   tostring = function(val) return ipv4_ntop(val[1])..'/'..tostring(val[2]) end
+   tostring = function(val)
+    return ipv4:ntop(val.prefix)..'/'..tostring(val.len) 
+   end
 }
 
 types['ipv6-prefix'] = {
    ctype = 'struct { uint8_t prefix[16]; uint8_t len; }',
    parse = function(str, what)
       local prefix, len = str:match('^([^/]+)/(.*)$')
-      return { assert(ipv6:pton(prefix)), util.tointeger(len, 1, 128) }
+      return { assert(ipv6:pton(prefix)), util.tointeger(len, 'len', 1, 128) }
    end,
-   tostring = function(val) return ipv6:ntop(val[1])..'/'..tostring(val[2]) end
+   tostring = function(val) return ipv6:ntop(val.prefix)..'/'..tostring(val.len) end
 }
 
 function selftest()
